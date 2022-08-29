d109d65f3b1d94db3b670880ccbb57e93721f26e
Viet0308 <phamviet0308@gmail.com>
2022-08-29 11:08:00 +0700

    update case

--- viet.js
+++ viet.js
@@ -1,42 +1,6 @@
-// class Product {
-//     name;
-//     price;
-//
-//     constructor(name, price) {
-//         this.name = name;
-//         this.price = price;
-//     }
-// }
-//
-// let cafe = new Product("cafe", 35000);
-// let socola = new Product("socola", 45000);
-// let bo = new Product("bo", 45000);
-// let chanh = new Product("chanh", 30000);
-// let suachua = new Product("suachua", 35000);
-//
-// let products = [cafe, socola, bo, chanh, suachua];
-// let indexEdit = -1;
-//
-// function show() {
-//     let str = "";
-//     for (let i = 0; i < products.length; i++) {
-//         str += `
-//               <tr>
-//                 <td>${products[i].name}</td>
-//                 <td>${products[i].price}</td>
-//                 <td><button onclick="deleteProduct(${i})">Delete</button></td>
-//             </tr>`
-//     }
-//     document.getElementById("show").innerHTML = str;
-// }
-//
-// function deleteProduct(index) {
-//     products.splice(index, 1);
-//     show();
-// }
 /* Khi nhấn nút Đặt Hàng, trả ra giá trị IMG, Name, Price */
-const btn = document.querySelectorAll("button")
-btn.forEach(function(button,index) {
+const btns = document.querySelectorAll("button")
+btns.forEach(function(button,index) {
     button.addEventListener("click", function (event) {
         let btnItem = event.target
         let product = btnItem.parentElement
@@ -53,8 +17,8 @@ btn.forEach(function(button,index) {
         let trcontent = '<tr>\n' +
             '                        <td style="display: flex;align-items: center;"><img style="width:70px" src="'+productImg+'" alt="">'+productName+'</td>\n' +
             '                        <td><p><span>'+productPrice+'</span><sup>đ</sup></p></td>\n' +
-            '                        <td><input style="width: 30px;outline: none;" type="number" value="1" min="1"></td>\n' +
-            '                        <td style="cursor: pointer"><input type="button" value="Xóa" onclick="deleteCart(this)"></td>\n' +
+            '                        <td><input style="width: 30px;outline: none;" type="number" value="1"  min="1" onchange="getElementById(\'result\').value=carttotal()"></td>\n' +
+            '                        <td style="cursor: pointer"><input type="button" value="Xóa" onclick="deleteCart(this);getElementById(\'result\').value=carttotal()"></td>\n' +
             '                    </tr>'
         addtr.innerHTML = trcontent //gán ngược lại các giá trị được gọi vào thẻ chờ.
         let cartTable = document.querySelector("tbody");
@@ -68,22 +32,12 @@ btn.forEach(function(button,index) {
         for (let i = 0; i < cartItem.length; i++) {
             let inputValue = cartItem[i].querySelector("input").value
             let productPrice = cartItem[i].querySelector("span").innerHTML
-            // console.log(inputValue, productPrice)
             sum += inputValue * productPrice * 1000
-                console.log(sum)
         }
         return document.getElementById("result").innerHTML = sum;
     }
-
-
-
-    //     let cartTotalA = document.querySelector(".price-total span")
-    //     cartTotalA.innerHTML = sum;
-    // }
-
     function deleteCart(r) {
         const i = r.parentNode.parentNode.rowIndex;
-        console.log("iiiiiiii", i)
         document.getElementById("myTable").deleteRow(i);
     }
 
